a6faae6026280663bc787db3b0199de11fc03d6d
Bryan de Paz <bryanascuc7@gmail.com>
2026-04-26 18:10:48 -0600

    Adding CORS policies

--- public/index.php
+++ public/index.php
@@ -2,6 +2,15 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Manejo del preflight (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 $router = require_once __DIR__ . '/../routes/api.php';
 
